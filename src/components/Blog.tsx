88cc460dd39f62dbc0cf81bf567b79bfbe6cac38
Gonza Nardini <nardinigonzalom@gmail.com>
2023-04-22 01:17:50 -0300

    Fetch blog titles

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Post } from '../pages';
 
-const Blog: React.FC = () => {
-  return <div></div>;
+interface BlogProps {
+  posts: Post[];
+}
+
+const Blog: React.FC<BlogProps> = ({ posts }) => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Blog Posts</h2>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index} className="mb-2">
+            <span className="font-semibold">{post.title}</span> -{' '}
+            <span className="text-gray-500">{post.date}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Blog;
-
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 import Landing from '../components/Landing';
 import Blog from '../components/Blog';
 
-export default function Home() {
+export interface Post {
+  title: string;
+  date: string;
+  content: string;
+}
+
+interface HomeProps {
+  posts: Post[];
+}
+
+const Home: React.FC<HomeProps> = ({ posts }) => {
   return (
     <div className="container mx-auto mt-12 px-4">
       <Landing />
-      <Blog />
+      <Blog posts={posts} />
     </div>
   );
+};
+
+export async function getStaticProps() {
+  const response = await axios.get(
+    'https://raw.githubusercontent.com/gnardini/blog-content/main/posts.json'
+  );
+  const posts: Post[] = response.data;
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
+
+export default Home;
+
