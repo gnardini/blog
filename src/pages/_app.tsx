0cace71901aeeae381a446e622b0b097f2a7a4ea
Gonza Nardini <nardinigonzalom@gmail.com>
2023-04-28 16:22:32 -0300

    Add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/Header";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,19 @@ export default function App({ Component, pageProps }: AppProps) {
           content="Gonza Nardini's personal website and blog"
         />
       </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-CB93D92PF4"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-CB93D92PF4');
+        `}
+      </Script>
       <Header />
       <Component {...pageProps} />
     </>
