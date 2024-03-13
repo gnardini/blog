a05828485c7cd513d3a1bcf73a02dc076f916e67
Gonza Nardini <nardinigonzalom@gmail.com>
2024-03-13 16:33:21 -0300

    x

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,42 +12,81 @@ const About: React.FC = () => {
         Hi! I'm Gonza, a Software Engineer who likes building useful products.
         I've worked at big and small companies and realized what I really want
         is to own what I build and have as much freedom as possible, so I've
-        started building my own products. They don't pay my bills yet, but I'll
-        keep trying :)
+        started building my own products.
       </p>
-      <p className="text-lg text-start self-start">
-        Some of the projects I've built include:
-        <br />
-        <a
-          href="https://kamaraapp.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500 hover:underline"
-        >
-          Kamara
-        </a>{" "}
-        - a VS Code plugin that writes code for you.
-        <br />
-        <a
-          href="https://aram.zone"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500 hover:underline"
-        >
-          ARAM Zone
-        </a>{" "}
-        - a stats website for the ARAM game mode of League of Legends.
-        <br />
-        <a
-          href="https://celotracker.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500 hover:underline"
-        >
-          Celo Tracker
-        </a>{" "}
-        - a portfolio management platform for the Celo blockchain
-      </p>
+      <div className="w-full mt-2 flex flex-col items-start text-lg">
+        <p className="">Some of the projects I've built so far include:</p>
+        <ul className="gap-2">
+          <li>
+            <a
+              href="https://locut.io?ref=gnardini"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              Locutio
+            </a>{" "}
+            - a system that helps translate websites into several languages with
+            help of AI.
+          </li>
+          <li>
+            <a
+              href="https://mypathways.com?ref=gnardini"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              Pathways
+            </a>{" "}
+            - a flexible and fully-featured LMS that empowers companies and
+            academies to optimize their learning processes.
+          </li>
+          <li>
+            <a
+              href="https://lolsociety.com?ref=gnardini"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              LoL Society
+            </a>{" "}
+            - a stats website for League of Legends.
+          </li>
+          <li>
+            <a
+              href="https://kamaraapp.com?ref=gnardini"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              Kamara
+            </a>{" "}
+            - a VS Code plugin that writes code for you.
+          </li>
+          <li>
+            <a
+              href="https://aram.zone?ref=gnardini"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              ARAM Zone
+            </a>{" "}
+            - a stats website for the ARAM game mode of League of Legends.
+          </li>
+          <li>
+            <a
+              href="https://celotracker.com?ref=gnardini"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              Celo Tracker
+            </a>{" "}
+            - a portfolio management platform for the Celo blockchain
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
