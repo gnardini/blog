e9ccce1dc34808cc760b76f1d7fabeacba94d230
Gonza Nardini <nardinigonzalom@gmail.com>
2024-08-19 16:03:03 -0300

    favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,23 @@ import Head from 'next/head';
 import { AnalyticsProvider } from '../utils/analyticsContext';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const title = 'Gonza Nardini';
+  const description = "Gonza Nardini's personal website and blog";
   return (
     <>
       <Head>
-        <title>Gonza Nardini</title>
-        <meta name="description" content="Gonza Nardini's personal website and blog" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content={`/profile-pic.jpg`} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:image" content={`/profile-pic.jpg`} />
+        <meta name="twitter:card" content="summary" />
       </Head>
       <AnalyticsProvider>
         <>
